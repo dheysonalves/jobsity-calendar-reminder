c8c38f8cce168e71a563f7229fe5aa392b88068f
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-03-12 17:51:29 -0300

    feat: create first calendar structure

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import calendarReducer from '../components/calendar/calendarSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    calendar: calendarReducer,
   },
 });
--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 
+import './Calendar.scss';
+
 export default function Calendar() {
+	const [dateObject] = useState(moment());
 	const weekdayshort = moment.weekdaysShort();
 
 	const weekdayshortname = weekdayshort.map(day => {
@@ -12,14 +15,79 @@ export default function Calendar() {
 		);
 	});
 
+	const monthDays = (() => {
+		return dateObject.daysInMonth();
+	});
+
+	const currentDay = () => {
+		return dateObject.format("D");
+	};
+
+	const firstDayOfMonth = (() => {
+		let firstDay = moment(dateObject)
+			.startOf("month")
+			.format("d");
+		return firstDay;
+	});
+
+	const getBlanksFields = (() => {
+		let blanks = [];
+		for (let index = 0; index < firstDayOfMonth(); index++) {
+			blanks.push(
+				<td className="calendar-day empty">{""}</td>
+			);
+		}
+		return blanks;
+	});
+
+	const getMonthsDaysFields = (() => {
+		let daysInMonth = [];
+		for (let day = 1; day <= monthDays(); day++) {
+			let current = day == currentDay() ? "today" : "";
+
+			daysInMonth.push(
+				<td key={day} className={`calendar-day ${current}`}>
+					{day}
+				</td>
+			);
+		}
+		return daysInMonth;
+	});
+
+	const monthDaysSlots = (() => {
+		var totalSlots = [...getBlanksFields(), ...getMonthsDaysFields()];
+		let rows = [];
+		let cells = [];
+
+		totalSlots.forEach((row, i) => {
+			if (i % 7 !== 0) {
+				cells.push(row); // if index not equal 7 that means not go to next week
+			} else {
+				rows.push(cells); // when reach next week we contain all td in last week to rows
+				cells = []; // empty container
+				cells.push(row); // in current loop we still push current row to new container
+			}
+			if (i === totalSlots.length - 1) { // when end loop we add remain date
+				rows.push(cells);
+			}
+		});
+
+		return rows;
+	});
+
+	let daysinmonth = monthDaysSlots().map((day, index) => {
+		return <tr key={index}>{day}</tr>;
+	});
+
 	return (
-		<div>
-			<table>
-				<thead>
+		<div className="container">
+			<table className='table-container'>
+				<thead className="calendar-thead">
 					<tr>
 						{weekdayshortname}
 					</tr>
 				</thead>
+				<tbody>{daysinmonth}</tbody>
 			</table>
 		</div>
 	);
