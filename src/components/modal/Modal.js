4bdea53c7d254fc2e3607b84614091c391e141e3
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-03-13 19:35:55 -0300

    feat: update the modal component

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactModal from 'react-modal';
 import PropTypes from "prop-types";
+import { MdClose } from "react-icons/md";
 
 import './Modal.scss';
 
@@ -8,14 +9,16 @@ export default function Modal({ showModal, handleShowModal, children }) {
 	return (
 		<ReactModal
 			isOpen={showModal}
-			contentLabel="Minimal Modal Example"
+			contentLabel="Form add reminder modal"
 			onRequestClose={handleShowModal}
 			shouldCloseOnOverlayClick={true}
 			appElement={document.getElementById('root')}
-			className="Modal"
+			className="modal-container"
 		>
+			<div className="container-close-btn">
+				<MdClose onClick={handleShowModal} color="#EB5757" size={28} className="cursor-pointer" />
+			</div>
 			{children}
-			<button onClick={handleShowModal}>Close Modal</button>
 		</ReactModal>
 	);
 }
