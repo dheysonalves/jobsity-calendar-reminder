84ec7db659abb5a4053ddfe4b43bdb17579055e8
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-03-13 19:34:59 -0300

    fix: update the weather endpoint

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,19 +1,21 @@
 /* eslint-disable no-undef */
 import WeatherMapInstance from "./config/WeatherMapInstance";
 
-const getWeatherConditions = ((cityName) => {
-		if (cityName === undefined) throw Error();
-		WeatherMapInstance.get(`/weather`, {
+const getWeatherConditions = async (cityName) => {
+	if (cityName === undefined) throw Error();
+	try {
+		const response = await WeatherMapInstance.get(`/weather`, {
 			params: {
 				q: cityName,
 				appid: process.env.REACT_APP_OPEN_WEATHER_API_KEY
 			}
-		}).then(function (response) {
-			return response.data.weather;
-		}).catch(function (error) {
-			console.log(error.toJSON());
 		});
-});
+
+		return response.data.weather;
+	} catch (error) {
+		console.log(error.toJSON());
+	}
+};
 
 
 export {
