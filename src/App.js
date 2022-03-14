08f8d67bcd7f096493beb1ea4fd3ec0c74c25be1
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-03-14 04:13:29 -0300

    style: update general styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Calendar from './components/calendar/Calendar';
-
+import useCurrentDateTime from './hooks/useCurrentDateTime';
 import './App.scss';
 
 function App() {
+  const {currentMonthName} = useCurrentDateTime();
 
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{currentMonthName}</h1>
         <Calendar />
       </header>
     </div>
