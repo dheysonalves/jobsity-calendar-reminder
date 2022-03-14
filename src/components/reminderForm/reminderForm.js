967ed6ce6133e178dbc2ef1f469c41bf2ac3e2bf
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-03-14 02:25:05 -0300

    feat: update the controlled form

--- src/components/reminderForm/reminderForm.js
+++ src/components/reminderForm/reminderForm.js
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
+import PropTypes from "prop-types";
+
 import { dateWeatherConditionsThunk, selectCalendar } from '../calendar/calendarSlice';
-import { selectReminder, incrementReminder } from './reminderFormSlice';
+import { selectReminder, incrementReminder, updateReminder } from './reminderFormSlice';
 
 import Button from '../button/Button';
 import useCurrentDateTime from '../../hooks/useCurrentDateTime';
 import './reminderForm.scss';
 
-const Reminderform = () => {
-	const { weather, weatherStatus } = useSelector(selectCalendar);
-	const { reminders } = useSelector(selectReminder);
-
+const Reminderform = ({ reminderData }) => {
+	const { weather } = useSelector(selectCalendar);
+	const { reminders, newReminders } = useSelector(selectReminder);
 	const dispatch = useDispatch();
-	const [reminder, updateReminder] = useState({
+
+	const [reminder, setUpdateReminder] = useState({
 		reminder: '',
 		city: '',
 		date: '',
@@ -29,35 +31,52 @@ const Reminderform = () => {
 		const value = target.value;
 		const name = target.name;
 
-		updateReminder({
+		setUpdateReminder({
 			...reminder,
 			[name]: value
 		});
 	};
 
-	const handleSubmit = React.useCallback((event) => {
+	const updateSubmit = React.useCallback(async (event) => {
 		event.preventDefault();
 
-		dispatch(dateWeatherConditionsThunk(reminder.city));
-		dispatch(incrementReminder({
+		await dispatch(dateWeatherConditionsThunk(reminder.city));
+		await dispatch(updateReminder({
 			id: moment(reminder.date).format('DD'),
 			...reminder,
-			weather: weather[0],
+			weather: weather[0] !== null ? weather[0] : null,
 		}));
-		updateReminder({
+	}, [reminder, updateReminder, dispatch, dateWeatherConditionsThunk, weather]);
+
+	const incrementSubmit = React.useCallback(async (event) => {
+		event.preventDefault();
+
+		await dispatch(dateWeatherConditionsThunk(reminder.city));
+		await dispatch(incrementReminder({
 			id: moment(reminder.date).format('DD'),
 			...reminder,
-			weather: weather[0],
+			weather: weather[0] !== null ? weather[0] : null,
+		}));
+		setUpdateReminder({
+			id: moment(reminder.date).format('DD'),
+			...reminder,
+			weather: weather[0] !== null ? weather[0] : null,
 		});
-	}, [reminder, dateWeatherConditionsThunk, incrementReminder, weather]);
+
+	}, [reminder, dateWeatherConditionsThunk, dispatch, incrementReminder, weather]);
 
 	React.useEffect(() => {
-		console.log(weather, weatherStatus);
-		console.log('local: ', reminder, 'store: ', reminders);
-	}, [weather, reminder, reminders]);
+		if (reminderData) {
+			setUpdateReminder(reminderData);
+		}
+	}, [reminderData, setUpdateReminder]);
+
+	React.useEffect(() => {
+		console.log('reminders: ', reminders, newReminders);
+	}, [reminders, newReminders]);
 
 	return (
-		<form className='form-container' onSubmit={(e) => handleSubmit(e)}>
+		<form className='form-container' onSubmit={(e) => reminderData ? updateSubmit(e) : incrementSubmit(e)}>
 			<div className="wrapper">
 				<div className='form-group'>
 					<label htmlFor='reminder-input' className='form-label'>Reminder</label>
@@ -65,11 +84,11 @@ const Reminderform = () => {
 				</div>
 				<div className='form-group'>
 					<label htmlFor='city-input' className='form-label'>City</label>
-					<input type="text" id='city-input' className='form-input' placeholder='Place your city' name="city" value={reminder.city} onChange={(e) => handleInputChange(e)}/>
+					<input type="text" id='city-input' className='form-input' placeholder='Place your city' name="city" value={reminder.city} onChange={(e) => handleInputChange(e)} />
 				</div>
 				<div className='form-group'>
 					<label htmlFor='date-input' className='form-label'>Date</label>
-					<input type="date" min={startOfMonth} max={endOfMonth} id='date-input' className='form-input' placeholder='Place your date' name="date" value={reminder.date || ''} onChange={(e) => handleInputChange(e)}/>
+					<input type="date" min={startOfMonth} max={endOfMonth} id='date-input' className='form-input' placeholder='Place your date' name="date" value={reminder.date || ''} onChange={(e) => handleInputChange(e)} />
 				</div>
 				<div className='form-group'>
 					<label htmlFor='time-input' className='form-label'>Time</label>
@@ -78,22 +97,31 @@ const Reminderform = () => {
 				<div className='form-group' >
 					<label className='form-label'>Color</label>
 					<div className='row'>
-						<input type="radio" id='color-green-input' className='form-input-radio' name="color" value="green" checked={reminder.color === 'green' || false} onChange={(e) => handleInputChange(e)}/>
+						<input type="radio" id='color-green-input' className='form-input-radio' name="color" value="green" checked={reminder.color === 'green' || false} onChange={(e) => handleInputChange(e)} />
 						<label htmlFor='color-green-input' className='form-label'>Green</label>
 					</div>
 					<div className='row'>
-						<input type="radio" id='color-yellow-input' className='form-input-radio' name="color" value="yellow" checked={reminder.color === 'yellow' || false} onChange={(e) => handleInputChange(e)}/>
+						<input type="radio" id='color-yellow-input' className='form-input-radio' name="color" value="yellow" checked={reminder.color === 'yellow' || false} onChange={(e) => handleInputChange(e)} />
 						<label htmlFor='color-yellow-input' className='form-label'>Yellow</label>
 					</div>
 					<div className='row'>
-						<input type="radio" id='color-red-input' className='form-input-radio' name="color" value="red" checked={reminder.color === 'red' || false} onChange={(e) => handleInputChange(e)}/>
+						<input type="radio" id='color-red-input' className='form-input-radio' name="color" value="red" checked={reminder.color === 'red' || false} onChange={(e) => handleInputChange(e)} />
 						<label htmlFor='color-red-input' className='form-label'>Red</label>
 					</div>
 				</div>
 			</div>
 			<Button type="submit" text='SUBMIT' color='success' />
+			{
+				reminderData && (
+					<Button type="submit" text='button' color='danger' />
+				)
+			}
 		</form>
 	);
+};
+
+Reminderform.propTypes = {
+	reminderData: PropTypes.object
 };
 
 export default Reminderform;
