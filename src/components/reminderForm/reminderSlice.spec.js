1dc6919c018fca375702ab5dbcc0bcf1acecdfa0
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-03-15 10:52:17 -0300

    test: update reminders test case

--- src/components/reminderForm/reminderSlice.spec.js
+++ src/components/reminderForm/reminderSlice.spec.js
@@ -1,19 +1,20 @@
 /* eslint-disable no-undef */
 import reminderReducer, {
 	createReminder,
+	updateReminder,
 	deleteReminder,
 } from './reminderFormSlice';
 
 describe('counter reducer', () => {
 	const initialState = {
 		reminders: [],
-		weatherStatus: 'idle',
 	};
 
 	const DATA = {
-		"id": "28",
+		"id": "1a683d51-f093-4397-b8ce-49b737f250ca",
 		"reminder": "Voyager Emissions Dashboard Rain",
 		"date": "2022-02-28",
+		"day": "28",
 		"time": "14:27",
 		"city": "London",
 		"color": "blue",
@@ -28,7 +29,6 @@ describe('counter reducer', () => {
 	it('should handle initial state', () => {
 		expect(reminderReducer(undefined, { type: 'unknown' })).toEqual({
 			reminders: [],
-			newReminders: [],
 		});
 	});
 
@@ -36,6 +36,32 @@ describe('counter reducer', () => {
 		const actual = reminderReducer(initialState, createReminder(DATA));
 
 		expect(actual.reminders).toEqual([DATA]);
+	});
+
+	it('should handle updateReminder', () => {
+		const customInitialState = {
+			reminders: [DATA],
+		};
+
+		const updateReminders = {
+			"id": "1a683d51-f093-4397-b8ce-49b737f250ca",
+			"reminder": "Voyager Emissions Dashboard Rain",
+			"date": "2022-02-29",
+			"day": "29",
+			"time": "14:29",
+			"city": "London",
+			"color": "blue",
+			"weather": {
+				"id": 500,
+				"main": "Rain",
+				"description": "light rain",
+				"icon": "10n"
+			}
+		};
+
+		const actual = reminderReducer(customInitialState, updateReminder(updateReminders));
+
+		expect(actual.reminders).toEqual([updateReminders]);
 	});
 
 	it('should handle deleteReminder', () => {
