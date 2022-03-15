d63ac2e1b53a8ce9250a31fc2ff0c355accd8836
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-03-15 10:41:15 -0300

    fix: remove unecessary comments

--- src/hooks/useCalendar.js
+++ src/hooks/useCalendar.js
@@ -40,15 +40,21 @@ function useCalendar(reminders, onDayClick) {
 
 	const getMonthsDaysFields = useCallback(() => {
 		let daysInMonth = [];
+		const remindersArray = [...reminders];
+		remindersArray.sort(function (a, b) {
+			return a.time.localeCompare(b.time);
+		});
+
 		for (let day = 1; day <= monthDays(); day++) {
 			let current = day === currentDay() ? "today" : "";
 			let beforeToday = day < currentDay() ? "empty" : "";
+
 
 			daysInMonth.push(
 				<td key={day} id={`calendar-day-${day}`} className={`calendar-day ${current} ${beforeToday}`}>
 					<span className="day-span">{day}</span>
 					{
-						reminders.filter(item => item.day === day.toString()).map((item, key) => {
+						remindersArray.filter(item => item.day === day.toString()).map((item, key) => {
 							const isBgYellow = item.color === 'yellow' ? 'yellow-case' : '';
 
 							return (
@@ -102,13 +108,13 @@ function useCalendar(reminders, onDayClick) {
 
 		totalSlots.forEach((row, index) => {
 			if (index % 7 !== 0) {
-				cells.push(row); // if index not equal 7 that means not go to next week
+				cells.push(row);
 			} else {
-				rows.push(cells); // when reach next week we contain all td in last week to rows
-				cells = []; // empty container
-				cells.push(row); // in current loop we still push current row to new container
+				rows.push(cells);
+				cells = [];
+				cells.push(row);
 			}
-			if (index === totalSlots.length - 1) { // when end loop we add remain date
+			if (index === totalSlots.length - 1) {
 				rows.push(cells);
 			}
 		});
